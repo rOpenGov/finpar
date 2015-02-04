0e8b785dfc549c02ca6ef21711246209e340f55b
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-02-04 22:16:20 +0200

    Initial implementation of query_kamu_api()

---
+++ R/kamu_api.R
@@ -0,0 +1,51 @@
+#' Query KaMu API against a given endpoint.
+#' 
+#' Function provides a single function to query KaMU API and an cache 
+#' implementation. It also defines the base URL scheme for the API. API resource
+#' parameters are provided as arguments.
+#' 
+#' @param endpoint A character URL defining the queried endpoint.
+#' @param query list of query parameters.
+#' @param cache  Sets the cachce mode: \code{TRUE} = Use cache if available and 
+#' save to cache, \code{FALSE} = Ignore cache if available and do not save to 
+#' cache, \code{"flush"} = Ignore cache if available and save to cache.
+#' 
+#' @return httr response object (JSON parsed to a list). 
+#' 
+#' @note Currently response meta data is not used for anything.
+#' 
+#' @importFrom httr GET
+#' @importFrom R.cache loadCache
+#' @importFrom R.cache saveCache
+#' 
+#' @keywords internal
+#' 
+#' @author Joona Lehtomaki <joona.lehtomaki@@gmail.com>
+#' 
+query_kamu_api <- function(endpoint, query=NULL, cache=FALSE) {
+  
+  # Base URL for KaMu API
+  base_url <- "http://dev.kansanmuisti.fi"
+  endpoint <- paste0("api/v1/", endpoint)
+  
+  key <- c(endpoint=endpoint, query)
+  r_content <- NULL
+  
+  if (cache == TRUE) {
+    r_content <- loadCache(key, suffix="finpar.Rcache")
+  }
+  if (!is.null(r_content)) {
+    message("Loaded cached data")
+  } else {
+      r <- GET(base_url, path = endpoint, query = query)
+  }
+  # Check the request succeeded
+  stop_for_status(r)
+  
+  r_content <- content(r)
+  
+  if (cache == TRUE || cache == 'flush') {
+    saveCache(r_content, key=key, suffix=.options$cache)
+  }
+  return(r_content[["objects"]])
+}
