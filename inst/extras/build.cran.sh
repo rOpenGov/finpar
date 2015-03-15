6d5305dfcf9733ec3f4beb3e8b58668456abdd1f
antagomir <leo.lahti@iki.fi>
2015-03-16 00:56:30 +0100

    version update

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -5,7 +5,7 @@
 #/usr/bin/R CMD build ../../ --no-build-vignettes
 /usr/bin/R CMD build ../../
 #/usr/bin/R CMD build ../../ 
-/usr/bin/R CMD check --as-cran finpar_0.0.2.tar.gz
-/usr/bin/R CMD INSTALL finpar_0.0.2.tar.gz
+/usr/bin/R CMD check --as-cran finpar_0.1.1.tar.gz
+/usr/bin/R CMD INSTALL finpar_0.1.1.tar.gz
 #/usr/bin/R CMD BATCH document.R
 
